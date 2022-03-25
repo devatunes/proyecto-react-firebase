5d84df8daeeb56bb1893aa3738cb099a640bcc67
Alvaro Atunez <alvaroatunez@gmail.com>
2022-03-25 03:12:32 -0500

    Eliminar 'use strict'

--- src/App.jsx
+++ src/App.jsx
@@ -168,8 +168,6 @@ function App() {
 
   // Example starter JavaScript for disabling form submissions if there are invalid fields
   (function () {
-    'use strict'
-
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
     var forms = document.querySelectorAll('.needs-validation')
 
