63e1b67bf16593ea26de762f70484fb4870c9cbd
Alvaro Atunez <alvaroatunez@gmail.com>
2022-03-25 03:01:37 -0500

    Mejoras en validación de campos

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 //import { specialCharMap } from '@testing-library/user-event/dist/keyboard';
 import React from 'react';
-import { firebase } from './Firebase';
+import { firebase } from './firebase';
 
 function App() {
   const [nombres, setNombres] = React.useState('')
@@ -38,10 +38,10 @@ function App() {
   const agregarAlumno = async (e) => {
     e.preventDefault()
 
-    if(!(nombres.trim()&&apellidos.trim()&&edad.trim()&&identificacion.trim()&&celular.trim()&&correo.trim()&&direccion.trim())){
+/*    if(!(nombres.trim()&&apellidos.trim()&&edad.trim()&&identificacion.trim()&&celular.trim()&&correo.trim()&&direccion.trim())){
       setError('Digite los datos faltantes')
       return
-    }
+    }*/
 
     try{
         const db = firebase.firestore()
@@ -165,13 +165,33 @@ function App() {
     setId('')
     setError(null)
   }
+
+  // Example starter JavaScript for disabling form submissions if there are invalid fields
+  (function () {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) { 
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+        }, false)
+        form.classList.add('was-validated')
+      })
+  })()
  
   return (
     <div className="container mt-5">
       <h1 className="text-center">ALUMNOS</h1>
       <hr />
       <div className="row">
-        <div className="col-8">
+        <div className="col-7">
           <h4 className="text-center">Listado de alumnos</h4>
           <ul className="list-group">
             {
@@ -189,78 +209,112 @@ function App() {
             }
           </ul>
         </div>
-        <div className="col-4">
+        <div className="col-5">
           <h4 className="text-center">
             {
               modoEdicion ? 'Editar Alumno' : 'Agregar Alumnno'
             }
           </h4>
-          <form onSubmit={modoEdicion ? editarAlumno : agregarAlumno}>
+          <form class="needs-validation" novalidate onSubmit={modoEdicion ? editarAlumno : agregarAlumno}>
             {
               error ? <span className='text-danger'>{error}</span> : null
             }
 
-            <input
-              type="text"
-              pattern="[A-Za-z ]{5,30}"
-              className='form-control mb-2'
-              placeholder='Ingrese nombres ej. Juan Luis'
-              onChange={e => setNombres(e.target.value)}
-              value = {nombres}
-            />
-            <input
-              type="text"
-              pattern="[A-Za-z ]{5,30}"
-              className='form-control mb-2'
-              placeholder='Ingrese apellidos ej. Perez Robles'
-              onChange={e => setApellidos(e.target.value)}
-              value = {apellidos}
-            />
-            <input
-              type="tel"
-              pattern='\d{1,2}'
-              maxlength="2"
-              min='1'
-              max='99'
-              className='form-control mb-2'
-              placeholder='Ingrese edad ej. 26'
-              onChange={e => setEdad(e.target.value)}
-              value = {edad}
-            />
-            <input
-              type="tel"
-              pattern='\d{7,10}'
-              maxlength="10"
-              className='form-control mb-2'
-              placeholder='Ingrese identificación ej. 1082989235'
-              onChange={e => setIdentificacion(e.target.value)}
-              value = {identificacion}
-            />
-            <input
-              type="tel"
-              pattern='^[3]\d{9}$'
-              maxlength="10"
-              className='form-control mb-2'
-              placeholder='Ingrese celular ej. 3003017525'
-              onChange={e => setCelular(e.target.value)}
-              value = {celular}
-            />
-            <input
-              type="email"
-              pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
-              className='form-control mb-2'
-              placeholder='Ingrese correo ej. alvaro@gmail.com'
-              onChange={e => setCorreo(e.target.value)}
-              value = {correo}
-            />
-            <input
-              type="text"
-              className='form-control mb-2'
-              placeholder='Ingrese dirección ej. CR 17 # 25 - 40'
-              onChange={e => setDireccion(e.target.value)}
-              value = {direccion}
-            />
+            <div class="form-floating mb-2">
+              <input
+                type="text"
+                pattern="[A-Za-z ]{5,30}"
+                className='form-control mb-2'
+                onChange={e => setNombres(e.target.value)}
+                value = {nombres}
+                id="floatingNombres"
+                required
+              />
+              <label for="floatingNombres">Ingrese sus nombres ej. Juan Luis</label>
+            </div>
             
+            <div class="form-floating mb-2">
+              <input
+                type="text"
+                pattern="[A-Za-z ]{5,30}"
+                className='form-control mb-2'
+                onChange={e => setApellidos(e.target.value)}
+                value = {apellidos}
+                id="floatingApellidos"
+                required
+              />
+              <label for="floatingApellidos">Ingrese sus apellidos ej. Perez Ruiz</label>
+            </div>
+
+            <div class="form-floating mb-2">
+              <input
+                type="text"
+                pattern='\d{1,2}'
+                maxlength="2"
+                min='1'
+                max='99'
+                className='form-control mb-2'
+                onChange={e => setEdad(e.target.value)}
+                value = {edad}
+                id="floatingEdad"
+                required
+              />
+              <label for="floatingEdad">Ingrese su edad ej. 26</label>
+            </div>
+
+            <div class="form-floating mb-2">
+              <input
+                type="tel"
+                pattern='\d{7,10}'
+                maxlength="10"
+                className='form-control mb-2'
+                onChange={e => setIdentificacion(e.target.value)}
+                value = {identificacion}
+                id="floatingIdentificacion"
+                required
+              />
+              <label for="floatingIdentificacion">Ingrese su identificación ej. 1082989235</label>
+            </div>
+
+            <div class="form-floating mb-2">
+              <input
+                type="tel"
+                pattern='^[3]\d{9}$'
+                maxlength="10"
+                className='form-control mb-2'
+                onChange={e => setCelular(e.target.value)}
+                value = {celular}
+                id="floatingCelular"
+                required
+              />
+              <label for="floatingCelular">Ingrese su celular ej. 3003017525</label>
+            </div>
+
+            <div class="form-floating mb-2">
+              <input
+                type="email"
+                pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+                className='form-control mb-2'
+                onChange={e => setCorreo(e.target.value)}
+                value = {correo}
+                id="floatingCorreo"
+                required
+              />
+              <label for="floatingCorreo">Ingrese su correo ej. alvaro@gmail.com</label>
+            </div>
+
+            <div class="form-floating mb-2">
+              <input
+                type="text"
+                className='form-control mb-2'
+                onChange={e => setDireccion(e.target.value)}
+                value = {direccion}
+                id="floatingDireccion"
+                required
+              />
+              <label for="floatingDireccion">Ingrese su dirección ej. CR 17 # 25 - 40</label>
+            </div>
+
 
             {
               modoEdicion ?
